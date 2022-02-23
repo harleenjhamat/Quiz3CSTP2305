1226720e9dbcc9ec73061ef5a24083bbb071f525
Harleen Jhamat <000435811@student.vcc.ca>
2022-02-23 10:08:44 -0800

    Quiz 3 initial folder

---
+++ Quiz3CSTP2305/Quiz3CSTP2305/Quiz3CSTP2305App.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz3CSTP2305App.swift
+//  Quiz3CSTP2305
+//
+//  Created by Harleen Jhamat on 2022-02-23.
+//
+
+import SwiftUI
+
+@main
+struct Quiz3CSTP2305App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
